8d9e4fb82282fa13a41fc8f9819b650d5a02e47a
Bob Carroll <bob.carroll@alum.rit.edu>
2020-05-31 16:48:56 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(name='libcfbf',
+      version='0.2',
+      description="An implementation of Microsoft's Compound File Binary Format",
+      url='https://github.com/rcarz/libcfbf',
+      author='Bob Carroll',
+      author_email='bob.carroll@alum.rit.edu',
+      py_modules=['libcfbf'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Office Suites',
+        'Topic :: Software Development :: Libraries'])
